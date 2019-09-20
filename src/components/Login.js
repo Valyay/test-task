acb8a079739ed51c91c36ed505c6a1892c303f0d
Евгений Валяев <valyay@MacBook-Pro-Evgenij.local>
2019-09-20 15:44:19 +0300

    Add login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,78 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
 import fakeAuth from "./fakeAuth.js";
 
-  class Login extends Component {
-    state = { redirectToReferrer: false };
-  
-    login = () => {
-      fakeAuth.authenticate(() => {
-        this.setState({ redirectToReferrer: true });
-      });
-    };
-  
-    render() {
-      let { from } = this.props.location.state || { from: { pathname: "/" } };
-      let { redirectToReferrer } = this.state;
-  
-      if (redirectToReferrer) return <Redirect to={from} />;
-  
-      return (
-        <div>
-          <p>You must log in to view the page at {from.pathname}</p>
-          <button onClick={this.login}>Log in</button>
-        </div>
-      );
-    }
-  }
-  
+class Login extends Component {
+	constructor(props) {
+		super(props);
 
-  
-  export default Login;
+		this.state = {
+			username: "",
+			password: "",
+			redirectToReferrer: false
+		};
+	}
+
+	login = () => {
+		fakeAuth.authenticate(() => {
+			this.setState({ redirectToReferrer: true });
+		});
+	};
+
+	validateForm() {
+		return this.state.username.length > 0 && this.state.password.length > 0;
+	}
+
+	handleChange = event => {
+		this.setState({
+			[event.target.id]: event.target.value
+        });
+	};
+
+	handleSubmit = event => {
+		event.preventDefault();
+	};
+
+	render() {
+		let { from } = this.props.location.state || { from: { pathname: "/" } };
+		let { redirectToReferrer } = this.state;
+
+		if (redirectToReferrer) return <Redirect to={from} />;
+
+		return (
+			<div>
+				<p>You must log in to view the page at {from.pathname}</p>
+				<form onSubmit={this.handleSubmit}>
+					<FormGroup controlId="username">
+						<FormLabel>Email</FormLabel>
+						<FormControl
+							autoFocus
+							type="text"
+							value={this.state.username}
+							onChange={this.handleChange}
+						/>
+					</FormGroup>
+					<FormGroup controlId="password">
+						<FormLabel>Password</FormLabel>
+						<FormControl
+							value={this.state.password}
+							onChange={this.handleChange}
+							type="password"
+						/>
+					</FormGroup>
+					<Button
+						onClick={this.login}
+						block
+						disabled={!this.validateForm()}
+						type="submit"
+					>
+						Login
+					</Button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default Login;
